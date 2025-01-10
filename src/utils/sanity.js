1f442302071c246387c95e2bfecbc7d53c5ce0d7
ayush606 <ayushkashyap606@gmail.com>
2025-01-10 11:33:40 +0530

    sort by title nav

--- src/utils/sanity.js
+++ src/utils/sanity.js
@@ -16,7 +16,7 @@ export const fetchCategoriesTitleAndUrl = async () => {
   const url = `${sanityBaseUrl}${encodeURIComponent(query)}`;
   const response = await fetch(url);
   const data = await response.json();
-  return data.result.map((title) => ({
+  return data.result.sort((a, b) => a.localeCompare(b)).map((title) => ({
     text: title,
     href: '/products/' + title.toLowerCase().replaceAll(' ', '-'),
   }));
